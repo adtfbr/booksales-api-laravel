20ae760ad0914436a4776e48a7a3088126cedc43
unknown <adit.aja061202@gmail.com>
2025-10-21 19:13:42 +0700

    tugas pertemuan 6 framework laravel

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,27 +6,29 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens; // <-- Add this line for API tokens
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    /** @use HasFactory<\Database\Factories\UserFactory>, HasApiTokens, Notifiable */
+    use HasFactory, HasApiTokens, Notifiable; // <-- And add HasApiTokens here
 
     /**
      * The attributes that are mass assignable.
      *
-     * @var list<string>
+     * @var array<int, string>
      */
     protected $fillable = [
         'name',
         'email',
         'password',
+        'role', // <-- Add 'role' to allow it to be mass-assigned
     ];
 
     /**
      * The attributes that should be hidden for serialization.
      *
-     * @var list<string>
+     * @var array<int, string>
      */
     protected $hidden = [
         'password',
@@ -34,16 +36,15 @@ class User extends Authenticatable
     ];
 
     /**
-     * Get the attributes that should be cast.
+     * The attributes that should be cast.
      *
-     * @return array<string, string>
+     * @var array<string, string>
      */
     protected function casts(): array
     {
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
-            'alamat' => 'array',
         ];
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -5,12 +5,22 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthorController;
 use App\Http\Controllers\GenreController;
 use App\Http\Controllers\BookController;
+use App\Http\Controllers\AuthController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Endpoint untuk Autentikasi (Publik)
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 
-Route::apiResource('authors', AuthorController::class);
-Route::apiResource('genres', GenreController::class);
-
+Route::apiResource('authors', AuthorController::class)->only(['index', 'show']);
+Route::apiResource('genres', GenreController::class)->only(['index', 'show']);
 Route::get('/books', [BookController::class, 'index']);
+
+Route::middleware(['auth:sanctum', 'admin'])->group(function () {
+    Route::post('authors', [AuthorController::class, 'store']);
+    Route::put('authors/{author}', [AuthorController::class, 'update']);
+    Route::delete('authors/{author}', [AuthorController::class, 'destroy']);
+
+    Route::post('genres', [GenreController::class, 'store']);
+    Route::put('genres/{genre}', [GenreController::class, 'update']);
+    Route::delete('genres/{genre}', [GenreController::class, 'destroy']);
+});
